c3cb237c3c946c88cf8b652e443ee4c8952a8379
dhanushkomari <dhanushkomari@gmail.com>
2021-04-28 15:35:44 +0530

    hii

--- RTProject/settings.py
+++ RTProject/settings.py
@@ -123,3 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+#MEDIA SETTINGS
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static', 'media')
+
+#BOOTSTRAP
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
